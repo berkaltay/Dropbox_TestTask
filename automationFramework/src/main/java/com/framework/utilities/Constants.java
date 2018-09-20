5e80b5e34bdbd8fec7f1c705ce47b9b473eb875b
berkaltay <34011363+berkaltay@users.noreply.github.com>
2018-09-20 13:43:12 +0300

    Update Constants.java

--- automationFramework/src/main/java/com/framework/utilities/Constants.java
+++ automationFramework/src/main/java/com/framework/utilities/Constants.java
@@ -2,10 +2,10 @@ package com.framework.utilities;
 
 public class Constants {
 
-    public static final String USERNAME = "wahrsager@gmail.com";
-    public static final String PASSWORD = "100b200A.";
+    public static final String USERNAME = "";
+    public static final String PASSWORD = "";
     public static final String TEST_FILE_PATH = System.getProperty("user.dir") + "/src/main/resources/test_file.txt";
-    public static final String ACCESS_TOKEN = "_B_Po_gaLvAAAAAAAAAARg3wEPCZXmeSIbKIytlaM2s6sLSpVELknTwfGPSueSjR";
+    public static final String ACCESS_TOKEN = "";
 
 
 }
